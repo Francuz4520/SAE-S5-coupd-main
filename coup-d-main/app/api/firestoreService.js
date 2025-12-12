52940364addc63ff5ee5bc9efd92fc25f1ba8a38
Francuz4520 <oskarjakubczyk010@gmail.com>
2025-12-12 16:49:03 +0100

    Creation de conversation, envoie et affichaege de messages

--- coup-d-main/app/api/firestoreService.js
+++ coup-d-main/app/api/firestoreService.js
@@ -1,5 +1,5 @@
 import { db } from "./Firestore";
-import { getDoc , getDocs , doc, updateDoc, collection, query, where, onSnapshot, orderBy } from "firebase/firestore";
+import { getDoc , getDocs , doc, updateDoc, collection, query, where, onSnapshot, orderBy, setDoc, addDoc, serverTimestamp } from "firebase/firestore";
 
 
 export async function getUserDocument(uid) {
@@ -43,11 +43,8 @@ export async function getConversationDocumentByParticipants(participantIds) {
     try {
         const ref = collection(db, "conversations");
 
-        let q = ref;
-
-        participantIds.forEach(uid => {
-            q = query(q, where("participants", "array-contains", uid));
-        });
+        // 1 seul array-contains autorisé
+        const q = query(ref, where("participants", "array-contains", participantIds[0]));
 
         const snap = await getDocs(q);
 
@@ -56,11 +53,13 @@ export async function getConversationDocumentByParticipants(participantIds) {
             ...doc.data(),
         }));
 
+        // Filtrage manuel côté JS
         const match = allMatches.find(conv =>
             conv.participants.length === participantIds.length &&
             participantIds.every(uid => conv.participants.includes(uid))
         );
-        console.log("getConversationByParticipants - match:", match);
+
+        console.log("match:", match);
         return match || null;
 
     } catch (error) {
@@ -86,4 +85,62 @@ export function listenMessagesForConversation(conversationId, callback) {
     });
 
     return unsubscribe;
+}
+
+export async function createConversationDocument(participantsID, senderId, firstMessageText) {
+    try {
+        // 1) Créer une conversation vide (pour générer son ID)
+        const convRef = await addDoc(collection(db, "conversations"), {
+            participants: participantsID,
+            lastMessage: null, // on ajoutera le message juste après
+        });
+
+        const conversationId = convRef.id;
+
+        // 2) Ajouter le premier message dans la sous-collection messages/
+        const msgRef = await addDoc(
+            collection(db, "conversations", conversationId, "messages"),
+            {
+                senderId,
+                text: firstMessageText,
+                type: "text",
+                createdAt: serverTimestamp(),
+            }
+        );
+
+        // 3) Mettre à jour lastMessage avec l'ID du message
+        await setDoc(
+            doc(db, "conversations", conversationId),
+            { lastMessage: msgRef.id },
+            { merge: true }
+        );
+    } catch (error) {
+        console.error("Erreur createConversation:", error);
+        throw error;
+    }
+}
+
+export async function sendMessageToConversation(conversationId, senderId, text, type = "text") {
+    try {
+        if (!conversationId || !senderId || !text) throw new Error("Paramètres manquants");
+
+        const messagesRef = collection(db, "conversations", conversationId, "messages");
+        const newMessage = {
+            senderId,
+            text,
+            type,
+            createdAt: serverTimestamp(),
+        };
+        const messageDocRef = await addDoc(messagesRef, newMessage);
+
+        const conversationRef = doc(db, "conversations", conversationId);
+        await updateDoc(conversationRef, {
+            lastMessage: text,
+            updatedAt: serverTimestamp(),
+        });
+
+    } catch (error) {
+        console.error("Erreur sendMessageToConversation:", error);
+        throw error;
+    }
 }
--- coup-d-main/app/screens/Chat.jsx
+++ coup-d-main/app/screens/Chat.jsx
@@ -1,34 +1,38 @@
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, StyleSheet, KeyboardAvoidingView, TextInput, TouchableOpacity, Platform, FlatList  } from "react-native";
 import { useEffect, useState } from "react";
 import Banner from "../components/Banner";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { getConversationDocument, getConversationDocumentByParticipants, getUserDocument, listenMessagesForConversation } from "../api/firestoreService";
+import { getConversationDocument, getConversationDocumentByParticipants, getUserDocument, listenMessagesForConversation, createConversationDocument, sendMessageToConversation } from "../api/firestoreService";
 import { getAuth } from "firebase/auth";
 
 export default function ChatScreen({navigation, route}) {
+    
     const { interlocutors = null, conversationID = null } = route.params || {};
     const [newChat, setNewChat] = useState(conversationID ? false : true);
     const [conversationId, setConversationId] = useState(conversationID || null);
     console.log("ChatScreen:", "newChat:", newChat);
-    const [currentUser, setCurrentUser] = useState(null);
+    const [currentUserID, setCurrentUserID] = useState(null);
     const [messages, setMessages] = useState([]); 
     const [chatMembers, setChatMembers] = useState([]);
+    let flatListRef;
 
     useEffect(() => {
         async function loadUsers() {
             try {
                 const user = JSON.parse(await AsyncStorage.getItem("user")) || getAuth().currentUser;
-                setCurrentUser(await getUserDocument(user.uid));
+                console.log("userID:", user.uid);
+                if (!user) {
+                    console.error("Impossible de récupérer l'utilisateur.");
+                    return;
+                }
+
+                setCurrentUserID(user.uid);
+
                 const interlocutorsData = await Promise.all(
-                    (interlocutors || []).map(async (element) => {
-                        const memberData = await getUserDocument(element);
-                        if (!memberData) console.log("Utilisateur non trouvé :", element);
-                        return memberData;
-                    })
+                    (interlocutors || []).map(getUserDocument)
                 );
 
-                const membersList = [...interlocutorsData.filter(Boolean)];
-                setChatMembers(membersList);
+                setChatMembers(interlocutorsData.filter(Boolean));
 
             } catch (error) {
                 console.error("Erreur loadUsers:", error);
@@ -39,57 +43,202 @@ export default function ChatScreen({navigation, route}) {
     }, []);
 
     useEffect(() => {
+        if (!currentUserID) return;
+
         async function loadConversation() {
-            let conversation = null;
+            try {
+                let conversation = null;
 
-            if (conversationID) {
-                conversation = await getConversationDocument(conversationID);
-            } else if (interlocutors && interlocutors.length > 0) {
-                if (!currentUser) return;
-                const participantIds = [currentUser.uid, ...interlocutors].filter(Boolean);
-                conversation = await getConversationDocumentByParticipants(participantIds);
-            }
+                if (conversationID) {
+                    conversation = await getConversationDocument(conversationID);
+                } else if (interlocutors?.length > 0 && newChat) {
+                    const participantIds = [currentUserID, ...interlocutors];
+                    conversation = await getConversationDocumentByParticipants(participantIds);
+                }
 
-            if (conversation) {
-                setConversationId(conversation.id);
-                console.log("Conversation trouvée :", conversation.id);
-                setNewChat(false);
+                if (conversation) {
+                    setConversationId(conversation.id);
+                    setNewChat(false);
+                }
+
+            } catch (error) {
+                console.error("Erreur loadConversation:", error);
             }
         }
 
         loadConversation();
-    }, [currentUser, conversationID, interlocutors]);
+    }, [currentUserID, newChat]);
 
     useEffect(() => {
         if (newChat) return;
+        if (!conversationId) return;
 
         const unsubscribe = listenMessagesForConversation(conversationId, (msgs) => {
             setMessages(msgs);
         });
 
         return () => unsubscribe();
-    }, [newChat]);
-
+    }, [conversationId, newChat]);
 
     const chatMembersFirstname = chatMembers.map(m => m.firstname).join(", ");
+
+    const MessageInput = () => {
+        const [text, setText] = useState("");
+
+        const handleSend = () => {
+            if (text.trim().length === 0) return;
+            if (newChat && currentUserID && chatMembers.length > 0) {
+                const participants = [currentUserID, ...chatMembers.map(m => m.id)];
+                const senderId = currentUserID;
+                if (createConversationDocument(participants, senderId, text)) {
+                    console.log("Creating convo with", participants, senderId, text);
+                    console.log("Conversation créée avec succès");
+                    const conversation = getConversationDocumentByParticipants(participants);
+                    setConversationId(conversation.id);
+                    setNewChat(false);
+                } else {
+                    console.error("Échec de la création de la conversation");
+                }
+            }
+            else if (conversationId) {
+                console.log("Envoi du message dans la conversation existante:", conversationId, text);
+                sendMessageToConversation(conversationId, currentUserID, text);
+            }
+            else console.error("Impossible d'envoyer le message : données manquantes pour une nouvelle conversation");
+            setText("");
+        };
+
+        return (
+            <KeyboardAvoidingView 
+                behavior={Platform.OS === "ios" ? "padding" : "height"}
+                style={styles.inputContainer}
+            >
+                <View style={styles.innerContainer}>
+                    <TextInput
+                        style={styles.input}
+                        value={text}
+                        onChangeText={setText}
+                        placeholder="Écrire un message..."
+                        multiline
+                    />
+
+                    <TouchableOpacity onPress={handleSend} style={styles.button}>
+                        <Text style={styles.buttonText}>Envoyer</Text>
+                    </TouchableOpacity>
+                </View>
+            </KeyboardAvoidingView>
+        );
+    }
+
+    const MessageBubble = ({ message, currentUserMessage }) => {
+        return (
+            <View style={[styles.messageBubble, currentUserMessage ? styles.currentUserMessage : styles.otherMessage]}>
+                <Text style={styles.messageText}>{message.text}</Text>
+            </View>
+        );
+    }
 
     if (chatMembersFirstname) {
         return (
             <View style={styles.container}>
                 <Banner text={`Conversation avec ${chatMembersFirstname}`} onBack={() => navigation.navigate("Home", {screen: "Messages"})} />
                 {newChat ? (
-                    <Text>Commencer la conversation avec {chatMembersFirstname}</Text>
+                    <View style={styles.newConversationContainer}>
+                        <Text>Commencer la conversation avec {chatMembersFirstname}</Text>
+                        <MessageInput />
+                    </View>
                 ) : (
-                    <View>
+                    <View style={{ flex: 1 }}>
+                        <FlatList
+                            data={messages}
+                            keyExtractor={(item) => item.id}
+                            renderItem={({ item }) => (
+                                <MessageBubble 
+                                    message={item} 
+                                    currentUserMessage={item.senderId === currentUserID} 
+                                />
+                            )}
+                            contentContainerStyle={styles.messagesContainer}
+                            ref={(ref) => { flatListRef = ref }}
+                            onContentSizeChange={() => flatListRef?.scrollToEnd({ animated: true })}
+                            onLayout={() => flatListRef?.scrollToEnd({ animated: true })}
+                        />
+                        <MessageInput />
                     </View>
                 )}
             </View>
         );
     }
+
 }
 
+
 const styles = StyleSheet.create({
-    container: {
+    container : {
         flex: 1,
+        backgroundColor: "#fff",
+    },
+    newConversationContainer: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    inputContainer: {
+        flex: 1,
+        position: "absolute",
+        bottom: 20,
+        width: "100%",
+        backgroundColor: "#fff",
+        paddingVertical: 6,
+        borderTopWidth: 1,
+        borderColor: "#ddd",
+    },
+    innerContainer: {
+        flexDirection: "row",
+        alignItems: "center",
+        paddingHorizontal: 10,
+    },
+    input: {
+        flex: 1,
+        minHeight: 40,
+        maxHeight: 120,
+        paddingHorizontal: 12,
+        paddingVertical: 8,
+        backgroundColor: "#f2f2f2",
+        borderRadius: 20,
+    },
+    button: {
+        marginLeft: 10,
+        backgroundColor: "#007bff",
+        paddingHorizontal: 14,
+        paddingVertical: 10,
+        borderRadius: 20,
+    },
+    buttonText: {
+        color: "#fff",
+        fontWeight: "bold",
+    },
+    messagesContainer: {
+        padding: 10,
+    },
+    messageBubble: {
+        maxWidth: "75%",
+        padding: 10,
+        marginVertical: 6,
+        borderRadius: 12,
+    },
+
+    currentUserMessage: {
+        alignSelf: "flex-end",
+        backgroundColor: "#ddd",
+    },
+
+    otherMessage: {
+        alignSelf: "flex-start",
+        backgroundColor: "#a8e6a1",
+    },
+
+    messageText: {
+        fontSize: 15,
     },
 });
